4e7025064fcfef51c8db12d103c1b73f98e3e399
Anagata-Cakrawala <88611212+Stevens0856@users.noreply.github.com>
2024-03-02 09:15:11 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ require('dotenv').config()
 
 //_______________________ ┏  Main Setting  ┓ _______________________\\
 
-creator = '乂𝘼𝙡𝙞𝙥乂' // Nama 
+creator = '乂Cakrawala乂' // Nama 
 port = 8080 //port host
-LimitApikey = 200 // Limit Apikey default
+LimitApikey = 99999 // Limit Apikey default
 
 //___________ ┏  Database Mongodb Setting  ┓ ___________\\
 
